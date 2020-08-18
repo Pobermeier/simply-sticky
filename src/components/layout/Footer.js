20b820a3eb6affca441b53bef7558096745ec780
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-08-19 00:40:53 +0200

    Updated footer text

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,14 +8,21 @@ const Footer = () => {
           <div className="col l6 s12">
             <h5 className="white-text">Noteworthy</h5>
             <p className="grey-text text-lighten-4">
-              Make notes and organize your thoughts.
+              Make notes and organize your thoughts. The easy way.
             </p>
           </div>
         </div>
       </div>
       <div className="footer-copyright">
-        <div className="container">
-          © 2020 Patrick Obermeier. All rights reserved.
+        <div className="container" style={{ textAlign: 'center' }}>
+          © 2020{' '}
+          <a
+            href="https://www.patrickobermeier.dev"
+            className="grey-text text-lighten-2"
+          >
+            Patrick Obermeier
+          </a>
+          . All rights reserved.
         </div>
       </div>
     </footer>
