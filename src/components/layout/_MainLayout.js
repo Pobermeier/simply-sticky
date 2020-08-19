8b93d10dae6b572b6e315ad9eade739a79b8d8dc
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-08-19 17:52:00 +0200

    Added conditional rendering based on auth state

--- src/components/layout/_MainLayout.js
+++ src/components/layout/_MainLayout.js
@@ -2,16 +2,14 @@ import React from 'react';
 import Navbar from '../navigation/Navbar';
 import Footer from './Footer';
 
-const MainLayout = ({ children }) => {
-  return (
-    <div id="wrapper">
-      <header>
-        <Navbar />
-      </header>
-      <main>{children}</main>
-      <Footer />
-    </div>
-  );
-};
+const MainLayout = ({ isAuthenticated, children, login, logout }) => (
+  <div id="wrapper">
+    <header>
+      <Navbar isAuthenticated={isAuthenticated} login={login} logout={logout} />
+    </header>
+    <main>{children}</main>
+    {!isAuthenticated && <Footer />}
+  </div>
+);
 
 export default MainLayout;
