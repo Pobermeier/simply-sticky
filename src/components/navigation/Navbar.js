fc3cb14fc37272f668e932b8fce98f2d1971dd9f
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-08-19 17:52:36 +0200

    Added conditional rendering based on auth state

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,38 +1,81 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => (
-  <nav>
-    <div className="nav-wrapper container">
-      <div className="row">
-        <div class="col s12 m6">
-          <a href="#" className="brand-logo">
-            <img
-              src={logo}
-              alt="Noteworthy"
-              style={{ display: 'inline', width: '1.75rem' }}
-              width="1.75rem"
-            />{' '}
-            Noteworthy
-          </a>
+const Navbar = ({ isAuthenticated, login, logout }) => {
+  if (isAuthenticated)
+    return (
+      <nav>
+        <div className="container">
+          <div className="row">
+            <div className="col s9">
+              <Link
+                to="/"
+                className="brand-logo left"
+                style={{ marginLeft: '1rem' }}
+              >
+                <img
+                  src={logo}
+                  alt="Noteworthy"
+                  style={{ width: '1.5rem' }}
+                  width="1.5rem"
+                />{' '}
+                <h1>Noteworthy</h1>
+              </Link>
+            </div>
+            <div className="col s3">
+              <ul className="right">
+                <li>
+                  <button
+                    onClick={logout}
+                    className="waves-effect waves-light btn grey-text text-darken-4 grey lighten-3"
+                  >
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div class="col m6">
-          <ul className="right hide-on-med-and-down">
-            <li>
-              <a href="#" className="waves-effect waves-light btn">
-                Register
-              </a>
-            </li>
-            <li>
-              <a href="#" className="waves-effect waves-light btn-flat">
-                Login
-              </a>
-            </li>
-          </ul>
+      </nav>
+    );
+  else
+    return (
+      <nav>
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m6">
+              <Link to="/" className="brand-logo">
+                <img
+                  src={logo}
+                  alt="Noteworthy"
+                  style={{ width: '1.5rem' }}
+                  width="1.5rem"
+                />{' '}
+                <h1>Noteworthy</h1>
+              </Link>
+            </div>
+            <div className="col m6">
+              <ul className="right hide-on-med-and-down button-group">
+                <li>
+                  <button className="waves-effect waves-light btn">
+                    Register
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={login}
+                    className="waves-effect waves-light btn grey-text text-darken-4 grey lighten-3"
+                  >
+                    Login
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </nav>
-);
+      </nav>
+    );
+};
 
 export default Navbar;
