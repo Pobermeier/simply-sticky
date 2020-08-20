2cf633b70a0a31d82215c68a2acd4bceb800552c
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-08-20 15:33:19 +0200

    Updated landing page styling

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -6,30 +6,34 @@ const Landing = ({ isAuthenticated, login, register }) => {
   else
     return (
       <div className="hero-img">
-        <div className="container hero-content">
-          <div className="row">
-            <div className="col s12">
-              <h2>The quick and easy note taking solution!</h2>
-              <h5>All your thoughts secure in one place.</h5>
+        <div className="hero-overlay">
+          <div className="container hero-content">
+            <div className="row">
+              <div className="col s12">
+                <h2 className="teal-text text-darken-1">
+                  The quick and easy note taking solution!
+                </h2>
+                <h5>All your thoughts secure in one place.</h5>
+              </div>
             </div>
-          </div>
 
-          <div className="row">
-            <div className="col s12 m6" style={{ marginBottom: '1rem' }}>
-              <button
-                onClick={register}
-                className="waves-effect waves-light btn-large"
-              >
-                Register
-              </button>
-            </div>
-            <div className="col s12 m6" style={{ marginBottom: '1rem' }}>
-              <button
-                onClick={login}
-                className="waves-effect waves-light btn-large grey-text text-darken-4 grey lighten-3"
-              >
-                Login
-              </button>
+            <div className="row">
+              <div className="col s12 m6" style={{ marginBottom: '1rem' }}>
+                <button
+                  onClick={register}
+                  className="waves-effect waves-light btn-large"
+                >
+                  Register
+                </button>
+              </div>
+              <div className="col s12 m6" style={{ marginBottom: '1rem' }}>
+                <button
+                  onClick={login}
+                  className="waves-effect waves-light btn-large grey-text text-darken-4 grey lighten-3"
+                >
+                  Login
+                </button>
+              </div>
             </div>
           </div>
         </div>
