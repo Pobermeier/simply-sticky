87ece6ac72202093dc0a0ba8c18aa12e0dbeedf3
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-08-25 10:19:30 +0200

    Added bg-image to 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -7,23 +7,27 @@ const NotFound = () => {
   }, []);
 
   return (
-    <div
-      style={{
-        padding: '0 2rem',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-      }}
-    >
-      <h2 className="teal-text text-darken-1">Oh noes! A big ol' 404!</h2>
-      <h5>This page doesn't exist. Nothing to see here.</h5>
-      <br />
-      <Link to="/" className="waves-effect waves-light btn-large">
-        Back to Homepage
-      </Link>
-      <br />
+    <div className="hero-img">
+      <div className="hero-overlay">
+        <div
+          style={{
+            padding: '0 2rem',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+          }}
+        >
+          <h2 className="teal-text text-darken-1">Oh noes! A big ol' 404!</h2>
+          <h5>This page doesn't exist. Nothing to see here.</h5>
+          <br />
+          <Link to="/" className="waves-effect waves-light btn-large">
+            Back to Homepage
+          </Link>
+          <br />
+        </div>
+      </div>
     </div>
   );
 };
