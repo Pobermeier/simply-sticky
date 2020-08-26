2a1ae86d38c7408db571b3272ffefbe580d6359e
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-08-26 10:11:42 +0200

    Implemented GA component to track client-side routing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import AppState from './context/global/AppState';
 import MainLayout from './components/layout/_MainLayout';
 import Routes from './components/routing/Routes';
 import './App.css';
+import GoogleAnalytics from './analytics/GoogleAnalytics';
 
 const App = () => (
   <AppState>
     <MainLayout>
       <Routes />
+      <GoogleAnalytics />
     </MainLayout>
   </AppState>
 );
--- src/analytics/GoogleAnalytics.js
+++ src/analytics/GoogleAnalytics.js
@@ -6,12 +6,11 @@ const GoogleAnalytics = ({ location, history }) => {
     const gtag = window.gtag;
 
     if (location.pathname === this.props.location.pathname) {
-      // don't log identical link clicks (nav links likely)
       return;
     }
 
     if (history.action === 'PUSH' && typeof gtag === 'function') {
-      gtag('config', GA_TRACKING_ID, {
+      gtag('config', 'UA-147439668-2', {
         page_title: document.title,
         page_location: window.location.href,
         page_path: location.pathname,
